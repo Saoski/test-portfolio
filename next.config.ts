07f97922afdaf67489ebed7f98585c232bde278d
Sean O. <sao3216@rit.edu>
2026-09-19 17:18:38 -0400

    Fix non loading images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "export",
     basePath: "/test-portfolio", // Change this to your exact GitHub repo name
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
