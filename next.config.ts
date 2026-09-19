c31a7f148432fa7546f45e60bf4f7eae2e5b2c71
Sean O. <sao3216@rit.edu>
2026-09-19 17:16:47 -0400

    Attempt to fix no css issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
+    basePath: "/test-portfolio", // Change this to your exact GitHub repo name
 };
 
 export default nextConfig;
